252d21d73cccc3c2869b2504346e4f94fb601367
vasques <laiss.hoga@aluno.ifsp.edu.br>
2025-12-02 14:24:38 -0300

    Modify usuarios table structure and data
    Updated the SQL script to modify the structure of the 'usuarios' table by adding an 'endereco' column and changing the 'senha' column to a larger varchar size. Adjusted the AUTO_INCREMENT value for the 'usuarios' table.

--- app/banco/dbsarah.sql
+++ app/banco/dbsarah.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 27/08/2025 às 04:37
+-- Tempo de geração: 02/12/2025 às 16:57
 -- Versão do servidor: 10.4.32-MariaDB
 -- Versão do PHP: 8.2.12
 
@@ -24,6 +24,81 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Estrutura para tabela `clientes`
+--
+
+CREATE TABLE `clientes` (
+  `id` int(11) NOT NULL,
+  `nome` varchar(45) NOT NULL,
+  `telefone` int(13) NOT NULL,
+  `endereco` varchar(255) NOT NULL,
+  `servico` varchar(45) NOT NULL,
+  `valor` int(10) NOT NULL,
+  `dia` date NOT NULL,
+  `hora` time NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `galeria`
+--
+
+CREATE TABLE `galeria` (
+  `id` int(11) NOT NULL,
+  `endereco` varchar(255) NOT NULL,
+  `posição` varchar(255) NOT NULL,
+  `descricao` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `imagem`
+--
+
+CREATE TABLE `imagem` (
+  `id` int(11) NOT NULL,
+  `projeto` varchar(255) NOT NULL,
+  `tipo` varchar(255) NOT NULL,
+  `endereco1` varchar(255) NOT NULL,
+  `endereco2` varchar(255) NOT NULL,
+  `endereco3` varchar(255) NOT NULL,
+  `endereco4` varchar(255) NOT NULL,
+  `endereco5` varchar(255) NOT NULL,
+  `descricao` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `imagem`
+--
+
+INSERT INTO `imagem` (`id`, `projeto`, `tipo`, `endereco1`, `endereco2`, `endereco3`, `endereco4`, `endereco5`, `descricao`) VALUES
+(90, 'dg', 'cabelo', '692ee980a50c8.jpg', '692ee980a54dd.jpg', '692ee980a5841.jpg', '692ee980a5c19.jpg', '692ee980a5ec6.jpg', 'xtnxdm'),
+(91, 'ffff', 'cabelo', '692ef96c54f72.jpg', '692ef96c5531b.jpg', '692ef96c5569d.jpg', '692ef96c55e33.jpg', '692ef96c56133.jpg', 'dgxgnd'),
+(92, 'ggg', 'maquiagem', '692ef9f0cc6df.jpg', '692ef9f155478.jpg', '692ef9f158e5c.jpg', '692ef9f163a56.jpg', '692ef9f1646de.jpg', 'xsdgszh'),
+(93, 'peles', 'peles', '692efacf818e1.jpg', '692efacf81c39.jpg', '692efacf8232a.jpg', '692efacf826e5.jpg', '692efacf82b13.jpg', 'cbcdew8');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `perfil`
+--
+
+CREATE TABLE `perfil` (
+  `id` int(11) NOT NULL,
+  `email` varchar(45) NOT NULL,
+  `senha` varchar(45) NOT NULL,
+  `telefone` int(13) NOT NULL,
+  `cep` int(8) NOT NULL,
+  `bairro` varchar(255) NOT NULL,
+  `rua` varchar(255) NOT NULL,
+  `numero` int(6) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Estrutura para tabela `usuarios`
 --
 
@@ -31,20 +106,45 @@ CREATE TABLE `usuarios` (
   `id` int(6) NOT NULL,
   `permissao` int(1) NOT NULL,
   `email` varchar(45) NOT NULL,
-  `senha` varchar(45) NOT NULL,
-  `nome` varchar(45) NOT NULL
+  `senha` varchar(255) NOT NULL,
+  `nome` varchar(45) NOT NULL,
+  `endereco` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Despejando dados para a tabela `usuarios`
 --
 
-INSERT INTO `usuarios` (`id`, `permissao`, `email`, `senha`, `nome`) VALUES
-(1, 3, 'admin@sarahelizabeth', 'sarah123@admin', 'Administradora');
+INSERT INTO `usuarios` (`id`, `permissao`, `email`, `senha`, `nome`, `endereco`) VALUES
+(9, 3, 'admin@sarahelizabeth', '$2y$10$6SR4g2PgTfu8.dTWq.LIiunD2jXbugCvqke3tsoRWgTzdWkbBirZK', 'Administradora', '');
 
 --
 -- Índices para tabelas despejadas
 --
+
+--
+-- Índices de tabela `clientes`
+--
+ALTER TABLE `clientes`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Índices de tabela `galeria`
+--
+ALTER TABLE `galeria`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Índices de tabela `imagem`
+--
+ALTER TABLE `imagem`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Índices de tabela `perfil`
+--
+ALTER TABLE `perfil`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Índices de tabela `usuarios`
@@ -57,10 +157,34 @@ ALTER TABLE `usuarios`
 --
 
 --
+-- AUTO_INCREMENT de tabela `clientes`
+--
+ALTER TABLE `clientes`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `galeria`
+--
+ALTER TABLE `galeria`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `imagem`
+--
+ALTER TABLE `imagem`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=94;
+
+--
+-- AUTO_INCREMENT de tabela `perfil`
+--
+ALTER TABLE `perfil`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT de tabela `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id` int(6) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(6) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
